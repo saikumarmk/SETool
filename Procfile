1e184a97d5a0813c6e336fdaff387e095d17da25
Sai kumar Murali krishnan <sai494iscool@gmail.com>
2021-01-03 12:29:37 +1100

    Procfile addition
    Added a procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn src/webapp_main:server
--- src/webapp_main.py
+++ src/webapp_main.py
@@ -19,6 +19,8 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css', dbc.themes
 # Meta tags help mobile render
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets,    meta_tags=[
                 {'name': 'viewport', 'content': 'width=device-width, initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.5'}])
+server = app.server
+
 
 PAGE_SIZE = 50
 
